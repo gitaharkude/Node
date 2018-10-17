593c2428ebb376139b20729a51818449a249cb05
gita h <gita@eizzy.in>
2018-10-17 06:12:38 -0700

    nodejs update forms

--- nodejs/server1.js
+++ nodejs/server1.js
@@ -1,16 +1,28 @@
 var http = require('http');
 var fs = require('fs');
-var path= require('path');
+var path = require('path');
 
 http.createServer(function(req,res){
-    if(req.url === "/"){
-    fs.readFile("./public/index.html/","UTF-8",function(error,html){
+
+if(req.url=== "/"){
+fs.readFile("./public/index.html/","UTF-8",function(error,html){
     res.writeHead(200,{'content-Type':'text/html'});
     res.end(html);
 });
-}else if(req.url.match("\.css$")){
-    var cssPath=path.join(__dirname, 'public',req.url);
-    console.log(cssPath);
+} else if(req.url.match("\.css$")){
+    var csspath = path.join(__dirname,'public',req.url);
+    var fileStream = fs.createReadStream(csspath,"UTF-8");
+    res.writeHead(200,{"content-Type":"text/css"});
+    fileStream.pipe(res);
+}else if(req.url.match("\.jpg$")){
+    var imagepath = path.join(__dirname,'public',req.url);
+    var fileStream = fs.createReadStream(imagepath);
+    res.writeHead(200,{"content-Type":"image/jpg"});
+    fileStream.pipe(res);
+}else{
+    res.writeHead(404,{'content-Type':'text/html'});
+    res.end("no page found");
+
 }
 
 }).listen(4000)
